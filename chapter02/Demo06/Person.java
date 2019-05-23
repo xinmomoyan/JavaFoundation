0d57fad9e361e3cd17ccebeb6043c490245ec54c
xinmomoyan <47499512+xinmomoyan@users.noreply.github.com>
2019-05-23 16:40:40 +0800

    Add files via upload

---
+++ chapter02/Demo06/Person.java
@@ -0,0 +1,9 @@
+package cn.jxufe.java.chapter2.demo06;
+
+public class Person {
+	int age;
+	public void speak() {
+		int age = 18;
+		System.out.println("age = " + this.age);
+	}
+}
---
+++ chapter02/Demo06/TestPerson.java
@@ -0,0 +1,13 @@
+package cn.jxufe.java.chapter2.demo06;
+
+public class TestPerson {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Person person = new Person();
+		person.age = 28;
+		person.speak();
+		System.out.println("person.age= " + person.age);
+	}
+
+}
